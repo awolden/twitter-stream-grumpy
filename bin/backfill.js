ca5e98f84989224fbcedf26cd80212a8ea3b0a2e
Alexander Wolden <awolden@gmail.com>
2015-02-21 16:48:06 -0800

    backfill now loads more than 100

--- bin/backfill.js
+++ bin/backfill.js
@@ -4,27 +4,47 @@ var Twit = require('twit'),
     config = require('../config'),
     Tweet = require('../models/tweet'),
     twit = new Twit(config.twit),
-    msgsToLoad = 100,
+    msgsToLoad = 5000,
     msgsPerCall = 100;
 
 
 //backfill set amount of messages into the application so we have a health dataset to work with
 //could theoretically load all relevant tweets but the api is rate limited.
 for (var i = msgsToLoad; i > 0; i -= msgsPerCall) {
-    twit.get('search/tweets', {
+
+}
+
+function getPage(msgs, count, maxId) {
+
+    if (msgs <= 0) {
+        console.log('complete');
+        return;
+    }
+
+    var paramsObj = {
         q: '#grumpycat OR #maruthecat',
         count: msgsPerCall
-    }, function (err, data, response) {
+    };
+
+    if (maxId) paramsObj.max_id = maxId;
+
+    twit.get('search/tweets', paramsObj, function (err, data, response) {
         if (err) console.log(err);
-        console.log(data)
+
+        console.log('tweets returned and processing ->', data.statuses.length);
+
+        //trigger next page load
+        getPage((msgs - count), count, data.statuses[data.statuses.length - 1].id);
+
         data.statuses.forEach(function (tweet) {
             Tweet.update({
                 id: tweet.id
             }, tweet, {
                 upsert: true
-            }, function (err, result) {
-                console.log('added tweet ->', result);
-            });
+            }, function (err, result) {});
         });
+
     });
 }
+
+getPage(msgsToLoad, msgsPerCall);
