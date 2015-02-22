3d48a3f2cb7753ad31de94877bde2710deed1d98
Alexander Wolden <awolden@gmail.com>
2015-02-22 12:41:03 -0800

    basic server side test complete

--- test/tweet-sockets.spec.js
+++ test/tweet-sockets.spec.js
@@ -6,11 +6,18 @@ var chai = require('chai'),
     assert = chai.assert,
     expect = chai.expect,
     mock = require('./lib/mock-db'),
-    db = mock.db;
+    TweetSocket = require('../lib/tweet-sockets'),
+    db = mock.db,
+    http = require('http');
 
+//chai support for promises
+chai.use(require("chai-as-promised"));
 
+//inject mock-db and dummy server
+var tweetSockets = new TweetSocket(http.createServer(), db);
 
 describe('Tweet Sockets', function (done) {
+
     before(function (done) {
         mock.createTweets(done);
     })
@@ -18,10 +25,44 @@ describe('Tweet Sockets', function (done) {
         mock.destroyTweets(done);
     })
 
-    it('Query the database correctly', function (done) {
+    //TODO: Mock socket connection and test
 
+    /*
+     * test public methods
+     */
+    it('Get a list of top posters', function () {
 
-        done();
+        //basic test of result length
+        expect(tweetSockets.getTopPosters()).to.eventually.have.length(3);
+
+        //TODO: test for properties and values on results
+
+    });
+
+    it('Get a total count of tweets', function () {
+
+        //basic test of count
+        expect(tweetSockets.getCount()).to.eventually.equal(3);
+
+    });
+
+    it('Get a list of tweets', function () {
+
+        //TODO: test more permutations of the sort/filter criteria
+        var params = {
+            "offset": 0,
+            "criteria": {
+                "sort": {
+                    "name": "Recent",
+                    "expr": "-id"
+                }
+            }
+        };
+
+        //basic test of result length
+        expect(tweetSockets.getTweets(params)).to.eventually.have.length(3);
+
+        //TODO: Test properties and values
 
     });
 });
